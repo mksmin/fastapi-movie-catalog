ba4bd3649d5ee57e812e5e807f1c259d68f6bb75
mks_min <mininname@vk.com>
2025-08-04 01:45:24 +0300

    Add create view for movie using form data

--- movie-catalog/api/api_v1/movies/views.py
+++ movie-catalog/api/api_v1/movies/views.py
@@ -1,11 +1,18 @@
+from annotated_types import (
+    Len,
+    Ge,
+    Le,
+)
 from fastapi import (
     Depends,
     APIRouter,
+    status,
+    Form,
 )
 from typing import Annotated
 
 from .crud import MOVIES
-from .dependencies import get_movie_by_id
+from .dependencies import get_movie_by_id, set_movie_id
 from schemas.movies import Movie
 
 router = APIRouter(
@@ -20,6 +27,34 @@ router = APIRouter(
 )
 def get_movies():
     return MOVIES
+
+
+@router.post("/", response_model=Movie, status_code=status.HTTP_201_CREATED)
+def create_movie(
+    movie_id: Annotated[int, Depends(set_movie_id)],
+    title: Annotated[
+        str,
+        Len(min_length=5, max_length=100),
+        Form(),
+    ],
+    description: Annotated[
+        str,
+        Len(min_length=10, max_length=250),
+        Form(),
+    ],
+    rating: Annotated[
+        int,
+        Ge(1),
+        Le(10),
+        Form(),
+    ],
+):
+    return Movie(
+        id=movie_id,
+        title=title,
+        description=description,
+        rating=rating,
+    )
 
 
 @router.get(
