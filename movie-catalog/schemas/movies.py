b63d048fbe7a485c32bc5df77514d082c6739c40
mks_min <mininname@vk.com>
2025-08-04 15:01:20 +0300

    Create ModelCreateSchema

--- movie-catalog/schemas/movies.py
+++ movie-catalog/schemas/movies.py
@@ -1,14 +1,38 @@
+from annotated_types import Le, Ge, Len
+from typing import Annotated
+
 from pydantic import BaseModel
 
 
 class MovieBase(BaseModel):
-    id: int
     title: str
     description: str
     rating: float
+
+
+class MovieCreateSchema(MovieBase):
+    """
+    Модель для создания фильма
+    """
+
+    title: Annotated[
+        str,
+        Len(min_length=5, max_length=100),
+    ]
+    description: Annotated[
+        str,
+        Len(min_length=10, max_length=250),
+    ]
+    rating: Annotated[
+        int,
+        Ge(1),
+        Le(10),
+    ]
 
 
 class Movie(MovieBase):
     """
     Модель для представления фильма в API.
     """
+
+    id: int
