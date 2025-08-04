cb021b533fccad5e0f3cf19cce4e111c78588b1a
mks_min <mininname@vk.com>
2025-08-04 15:08:17 +0300

    Remove id generator and change arg from movie_id to movie_slug

--- movie-catalog/api/api_v1/movies/dependencies.py
+++ movie-catalog/api/api_v1/movies/dependencies.py
@@ -7,10 +7,10 @@ from schemas.movies import Movie
 
 
 def get_movie_by_id(
-    movie_id: int,
+    movie_slug: str,
 ):
     movie: Movie | None = next(
-        (movie for movie in MOVIES if movie.id == movie_id),
+        (movie for movie in MOVIES if movie.slug == movie_slug),
         None,
     )
     if movie:
@@ -19,7 +19,3 @@ def get_movie_by_id(
         status_code=status.HTTP_404_NOT_FOUND,
         detail="Movie not found",
     )
-
-
-def set_movie_id() -> int:
-    return random.randint(1, 10000)
