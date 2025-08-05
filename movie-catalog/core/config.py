2036f5076e5ca94aa729205f517d59bd2448db9d
mks_min <mininname@vk.com>
2025-08-05 18:48:08 +0300

    Update state to .json file

--- movie-catalog/core/config.py
+++ movie-catalog/core/config.py
@@ -2,6 +2,7 @@ from pathlib import Path
 
 BASE_DIR = Path(__file__).resolve().parent.parent
 USER_DATA_STORAGE_DIR = BASE_DIR / "data_storage"
+USER_DATA_STORAGE_FILEPATH = USER_DATA_STORAGE_DIR / "user_data.json"
 
 if not USER_DATA_STORAGE_DIR.exists():
     USER_DATA_STORAGE_DIR.mkdir()
