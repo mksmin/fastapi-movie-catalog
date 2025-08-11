1c3fd1758da4ca2afcb24200f531814fe426aa15
mks_min <mininname@vk.com>
2025-08-11 22:16:31 +0300

    Create test for partial update movie schema

--- movie-catalog/testing/test_schemas/test_movies.py
+++ movie-catalog/testing/test_schemas/test_movies.py
@@ -1,4 +1,4 @@
-from schemas.movies import Movie, MovieCreate, MovieUpdate
+from schemas.movies import Movie, MovieCreate, MovieUpdate, MovieUpdatePartial
 from unittest import TestCase
 
 
@@ -61,6 +61,52 @@ class MovieUpdateTestCase(TestCase):
             movie_update.description,
             movie.description,
         )
+        self.assertEqual(
+            movie_update.rating,
+            movie.rating,
+        )
+
+
+class MoviePartialUpdateTestCase(TestCase):
+    def test_movie_can_be_updated_from_partial_update_schema(self) -> None:
+        movie_in = MovieCreate(
+            title="Test Movie",
+            description="Test Description",
+            rating=1,
+            slug="test-movie",
+        )
+        movie = Movie(
+            **movie_in.model_dump(),
+        )
+
+        movie_update = MovieUpdatePartial(
+            title="Updated Movie",
+        )
+        for field, value in movie_update.model_dump(exclude_unset=True).items():
+            setattr(movie, field, value)
+
+        self.assertEqual(
+            movie_update.title,
+            movie.title,
+        )
+
+        movie_update = MovieUpdatePartial(
+            description="",
+        )
+        for field, value in movie_update.model_dump(exclude_unset=True).items():
+            setattr(movie, field, value)
+
+        self.assertEqual(
+            movie_update.description,
+            movie.description,
+        )
+
+        movie_update = MovieUpdatePartial(
+            rating=3,
+        )
+        for field, value in movie_update.model_dump(exclude_unset=True).items():
+            setattr(movie, field, value)
+
         self.assertEqual(
             movie_update.rating,
             movie.rating,
