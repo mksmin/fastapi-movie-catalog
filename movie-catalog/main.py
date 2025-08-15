884d3bcbe65cb5d07d6b63a5f94728de439c16ef
mks_min <mininname@vk.com>
2025-08-16 00:05:01 +0300

    Move read_root to main_views.py

--- movie-catalog/main.py
+++ movie-catalog/main.py
@@ -3,10 +3,10 @@ import logging
 import uvicorn
 from fastapi import (
     FastAPI,
-    Request,
 )
 
 from api import router as api_router
+from api.main_views import router as main_views_router
 from app_lifespan import lifespan
 from core import config
 
@@ -19,25 +19,8 @@ app = FastAPI(
     title="Movie Catalog",
     lifespan=lifespan,
 )
-app.include_router(
-    api_router,
-)
-
-
-@app.get("/")
-def read_root(
-    request: Request,
-    name: str = "World",
-) -> dict[str, str]:
-    docs_url = request.url.replace(
-        path="/docs",
-        query="",
-    )
-    return {
-        "message": f"Hello {name}",
-        "docs": str(docs_url),
-    }
-
+app.include_router(api_router)
+app.include_router(main_views_router)
 
 if __name__ == "__main__":
     uvicorn.run("main:app", reload=True)
