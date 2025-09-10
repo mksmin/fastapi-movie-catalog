4e4d98c78849e0bc41b809fca72a8c44029eb41e
mks_min <mininname@vk.com>
2025-09-10 15:59:28 +0300

    Fix broken test

--- movie-catalog/testing/test_api/test_api_v1/test_movies/test_crud.py
+++ movie-catalog/testing/test_api/test_api_v1/test_movies/test_crud.py
@@ -115,5 +115,4 @@ def test_create_twice() -> None:
         match=movie_create.slug,
     ) as exc_info:
         storage.create_or_raise_if_exists(movie_create)
-        # special broke case
-        assert exc_info.value.args == (movie_create.slug,)
+    assert exc_info.value.args == (movie_create.slug,)
