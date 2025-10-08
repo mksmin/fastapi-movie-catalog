4309a8b5bc21187fb3682f1b33b8890c0a510d63
mks_min <mininname@vk.com>
2025-10-08 22:13:13 +0300

    Full form validation

--- movie-catalog/rest/movies/create_views.py
+++ movie-catalog/rest/movies/create_views.py
@@ -1,8 +1,8 @@
-from typing import Annotated, Any
+from collections.abc import Mapping
+from typing import Any
 
 from fastapi import (
     APIRouter,
-    Form,
     Request,
     status,
 )
@@ -10,6 +10,7 @@ from fastapi.responses import (
     HTMLResponse,
     RedirectResponse,
 )
+from pydantic import BaseModel, ValidationError
 
 from dependencies.movies import GetMoviesStorage
 from schemas.movies import MovieCreate
@@ -40,19 +41,54 @@ def get_page_create_movie(
     )
 
 
+def format_pydantic_error(
+    error: ValidationError,
+) -> dict[str, str]:
+    return {str(err["loc"][0]): err["msg"] for err in error.errors()}
+
+
+def create_view_validation_response(
+    request: Request,
+    errors: dict[str, str] | None = None,
+    form_data: BaseModel | Mapping[str, Any] | None = None,
+    *,
+    form_validated: bool = True,
+) -> HTMLResponse:
+    context: dict[str, Any] = {}
+    model_schema = MovieCreate.model_json_schema()
+    context.update(
+        model_schema=model_schema,
+        errors=errors,
+        form_validated=form_validated,
+        form_data=form_data,
+    )
+    return templates.TemplateResponse(
+        request=request,
+        name="movies/create.html",
+        context=context,
+        status_code=status.HTTP_422_UNPROCESSABLE_ENTITY,
+    )
+
+
 @router.post(
     "/",
     name="movies:create",
     response_model=None,
 )
-def create_movie(
+async def create_movie(
     request: Request,
-    movie_create: Annotated[
-        MovieCreate,
-        Form(),
-    ],
     storage: GetMoviesStorage,
 ) -> RedirectResponse | HTMLResponse:
+    async with request.form() as form:
+        try:
+            movie_create = MovieCreate.model_validate(form)
+        except ValidationError as e:
+            errors = format_pydantic_error(e)
+            return create_view_validation_response(
+                request=request,
+                errors=errors,
+                form_data=form,
+            )
     try:
         storage.create_or_raise_if_exists(
             movie_create,
@@ -66,17 +102,9 @@ def create_movie(
             url=request.url_for("movies:list"),
             status_code=status.HTTP_303_SEE_OTHER,
         )
-    context: dict[str, Any] = {}
-    model_schema = MovieCreate.model_json_schema()
-    context.update(
-        model_schema=model_schema,
-        errors=errors,
-        form_validated=True,
-        form_data=movie_create,
-    )
-    return templates.TemplateResponse(
+
+    return create_view_validation_response(
         request=request,
-        name="movies/create.html",
-        context=context,
-        status_code=status.HTTP_422_UNPROCESSABLE_ENTITY,
+        errors=errors,
+        form_data=movie_create,
     )
