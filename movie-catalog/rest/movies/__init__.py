4207ee713fc32ee82e0e664fa28e517afcf5e42f
mks_min <mininname@vk.com>
2025-10-12 13:30:09 +0300

    Require basic auth on form submit

--- movie-catalog/rest/movies/__init__.py
+++ movie-catalog/rest/movies/__init__.py
@@ -1,11 +1,15 @@
-from fastapi import APIRouter
+from fastapi import APIRouter, Depends
 
+from dependencies.auth import user_basic_auth_required_for_unsafe_methods
 from rest.movies.create_views import router as create_views_router
 from rest.movies.list_views import router as list_views_router
 
 router = APIRouter(
     tags=["Movies rest"],
     prefix="/movies",
+    dependencies=[
+        Depends(user_basic_auth_required_for_unsafe_methods),
+    ],
 )
 
 router.include_router(
