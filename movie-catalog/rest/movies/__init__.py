8877cb02cb6b0c7960341241511f49600f5400aa
mks_min <mininname@vk.com>
2025-10-21 04:06:07 +0300

    create delete movies view

--- movie-catalog/rest/movies/__init__.py
+++ movie-catalog/rest/movies/__init__.py
@@ -2,6 +2,7 @@ from fastapi import APIRouter, Depends
 
 from dependencies.auth import user_basic_auth_required_for_unsafe_methods
 from rest.movies.create_views import router as create_views_router
+from rest.movies.delete_views import router as delete_views_router
 from rest.movies.list_views import router as list_views_router
 from rest.movies.update_views import router as update_views_router
 
@@ -21,4 +22,7 @@ router.include_router(
 )
 router.include_router(
     update_views_router,
+)
+router.include_router(
+    delete_views_router,
 )
---
+++ movie-catalog/rest/movies/delete_views.py
@@ -0,0 +1,23 @@
+from fastapi import (
+    APIRouter,
+    status,
+)
+from fastapi.requests import Request
+from fastapi.responses import RedirectResponse
+
+router = APIRouter(
+    prefix="/{slug}/delete",
+)
+
+
+@router.post(
+    "/",
+    name="movies:delete",
+)
+def delete_movie(
+    request: Request,
+) -> RedirectResponse:
+    return RedirectResponse(
+        url=request.url_for("movies:list"),
+        status_code=status.HTTP_303_SEE_OTHER,
+    )
