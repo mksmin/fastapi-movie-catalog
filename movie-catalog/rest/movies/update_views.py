ad856738137f56a8f7986deca3ca9281ddb15018
mks_min <mininname@vk.com>
2025-10-21 03:27:49 +0300

    handle movie update validation errors

--- movie-catalog/rest/movies/update_views.py
+++ movie-catalog/rest/movies/update_views.py
@@ -1,10 +1,9 @@
-from typing import Annotated
-
-from fastapi import APIRouter, Form, Request, status
+from fastapi import APIRouter, Request, status
 from fastapi.responses import (
     HTMLResponse,
     RedirectResponse,
 )
+from pydantic import ValidationError
 
 from dependencies.movies import GetMoviesStorage, MovieBySlug
 from schemas.movies import MovieUpdate, MovieUpdateForm
@@ -39,16 +38,25 @@ def get_page_create_movie(
 @router.post(
     "/",
     name="movies:update",
+    response_model=None,
 )
 async def update_movie(
     request: Request,
     movie: MovieBySlug,
-    movie_in: Annotated[
-        MovieUpdateForm,
-        Form(),
-    ],
     storage: GetMoviesStorage,
-) -> RedirectResponse:
+) -> RedirectResponse | HTMLResponse:
+    async with request.form() as form:
+        try:
+            movie_in = MovieUpdateForm.model_validate(form)
+        except ValidationError as e:
+            return form_response.render(
+                request=request,
+                form_data=form,
+                form_validated=True,
+                pydantic_error=e,
+                movie=movie,
+            )
+
     storage.update(movie, movie_in)
     return RedirectResponse(
         url=request.url_for("movies:list"),
